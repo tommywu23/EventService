fe6c5c9e73b3abccb59e9fc61d35e6f9c4d1f370
tommywu23 <tommywu23@126.com>
2015-06-16 14:47:01 +0800

    Add

---
+++ routes/notify.js
@@ -0,0 +1,27 @@
+/**
+ * Created by tommy on 15/6/16.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+router.post('/notify', function(req, res, next) {
+    var p = req.body;
+    if(p.event == undefined || p == "") {
+        res.send(400);
+    }
+
+    console.log(p);
+
+    global.io.on('connection', function(socket){
+        socket.emit(p.event, p.params);
+    });
+
+    res.send(p);
+});
+
+router.get('/', function(req, res, next) {
+    res.send("EventService");
+});
+
+module.exports = router;
